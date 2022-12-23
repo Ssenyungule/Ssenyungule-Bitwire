343593952acfd19d71c80205ffa0a10ecef92683
Ssenyungule Herbert <88632439+Ssenyungule@users.noreply.github.com>
2022-12-23 13:21:26 +0300

    Added 1st batch of Js files
    Added 1st batch of Js files

---
+++ script03_02.js
@@ -0,0 +1,8 @@
+var negInteger = -3.1415782673838;
+var escQuote = "Quote can also be \"escaped\".";
+var theSunIsWarm = true;
+var emptyInside = null;
+var justAnotherVariable;
+
+// we use Console.log(typeof insertVaribleName)
+// to check for its data type
